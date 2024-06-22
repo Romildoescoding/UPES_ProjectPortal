89431b486ef187a4fbb3ee1140a2cbe8ce42f728
Romildoescoding <romilrajrana1@gmail.com>
2024-06-22 15:51:51 +0530

    Minor Changes in the Header Component

--- FRONTEND/project-portal/src/ui/DashboardHeader.jsx
+++ FRONTEND/project-portal/src/ui/DashboardHeader.jsx
@@ -4,20 +4,19 @@ import { useUser } from "../features/authentication/signin/useUser";
 function DashboardHeader() {
   const location = useLocation();
   const isStudent = location.pathname === "/student";
+  const isPanelMember = location.pathname === "/panel-members";
   const { data: session, isLoading } = useUser();
   const name = session?.user?.username;
   //   const role = session?.user?.role;
+  const headerText = isStudent
+    ? "Project Portal"
+    : isPanelMember
+    ? "Project Portal- Faculty Panel Members"
+    : "Project Portal- Activity Co-Ordinator";
   return (
     <div className="header">
       <img src="/images/upes-logo.png" alt="logo" className="logo-image" />
-      {isStudent ? (
-        <span className="header-span-dashboard">Project Portal</span>
-      ) : (
-        <span className="header-span-dashboard">
-          Project Portal- Activity Co-Ordinator
-        </span>
-      )}
-
+      <span className="header-span-dashboard">{headerText}</span>
       <div className="username-pill">
         <img
           src="/images/user-image.png"
