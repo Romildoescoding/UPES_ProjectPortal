7bbdfde35e5d0a54954f9f99e899790607ed2eca
Romildoescoding <romilrajrana1@gmail.com>
2024-10-23 02:03:15 +0530

    Updated the backend URL

--- client/src/helpers/backendApi.js
+++ client/src/helpers/backendApi.js
@@ -1,2 +1,2 @@
-export const serverPort = "http://localhost:3000";
+export const serverPort = "https://upes-project-portal.onrender.com";
 export const docServiceURL = "https://docs.google.com/viewer?url=";
